b8e4f47b0a3f8f560a1cbae7cd11a0d5d275e964
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-12 19:42:36 +0430

    Create a field to keep track of when to reshuffle the cards

--- Scripts/CardInitializer.cs
+++ Scripts/CardInitializer.cs
@@ -7,6 +7,7 @@ public class CardInitializer : MonoBehaviour
 
     [SerializeField] private Sprite[] images = new Sprite[4];
     private int[] numberOfTimesAnImageGotInit = new int[4];
+    private bool toRestart = true;
 
 
     void Start()
