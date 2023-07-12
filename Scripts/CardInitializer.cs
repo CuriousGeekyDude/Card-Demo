2debad30af939b0498beb3389f5d237041727b0f
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-12 19:39:40 +0430

    Create an array that keeps track of how many times each of the 4 images get initialized

--- Scripts/CardInitializer.cs
+++ Scripts/CardInitializer.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CardInitializer : MonoBehaviour
 {
 
-    [SerializeField] private Sprite[] images;
-    
+    [SerializeField] private Sprite[] images = new Sprite[4];
+    private int[] numberOfTimesAnImageGotInit = new int[4];
+
+
     void Start()
     {
         
