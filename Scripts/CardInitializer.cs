dfaa3d95664f1642abae8e09014876862ebaad36
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-13 20:22:44 +0430

    Keep track of which image appears in which pos by defining an array that contains imageID's

--- Scripts/CardInitializer.cs
+++ Scripts/CardInitializer.cs
@@ -6,6 +6,7 @@ public class CardInitializer : MonoBehaviour
 {
 
     [SerializeField] private Sprite[] images = new Sprite[4];
+    private int[] imageIdInScene = new int[8];
     [SerializeField] private CardToReveal card;
     private int[] numberOfTimesAnImageGotInit = new int[4];
     private bool toRestart = true;
