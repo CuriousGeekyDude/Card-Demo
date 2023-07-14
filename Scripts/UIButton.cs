ab35b14713c80c404ed2d366afb3d84444bcef47
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-15 03:33:05 +0430

    Add sceneController instance and functionToCall
    We need to be able to communicate with the object that controls the
    scene. After having access to that object's instance, we want to
    call one its methods in order to react to the player clicking on the
    button we just put on the screen.

--- Scripts/UIButton.cs
+++ Scripts/UIButton.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class UIButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject sceneController;
+    private string functionToCall;
+    
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
