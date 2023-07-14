0fc1e775aec64c87f7af063b71e32ff1c6a21e7a
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-14 20:57:11 +0430

    Initialize tuple in order to avoid compiler errors

--- Scripts/CardInitializer.cs
+++ Scripts/CardInitializer.cs
@@ -24,7 +24,7 @@ public class CardInitializer : MonoBehaviour
 
     private (int,int) FindIndicesOfImageID(int imageID)
     {
-        (int, int) indices;
+        (int, int) indices = (0, 0);
         int numberOfIndicesFound = 0;
         for(int i = 0; i < 8; ++i) {
             if(imageIdInScene[i] == imageID) {
